391929cf32c84487696083975109fa0af0ee3502
STEVEN JACHIMOWICZ <steven.jachimowicz@build.com>
2017-11-17 15:42:43 -0800

    stevedev added text copy challenge

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,26 @@
 define(['backbone',
 		'model/main',
-		'text!template/index-main.html'], function(Backbone, MainModel, IndexHTML) {
+		'text!template/index-challenge.html'], function(Backbone, MainModel, IndexHTML) {
 
 		var Application = Backbone.View.extend({
+
+			events: {
+				'keyup #typer': 'textCopy'
+			},
 
 			initialize: function() {
 				this.template = _.template(IndexHTML);
 				return Application.__super__.initialize.apply(this, arguments);
-
 			},
 
 			render: function() {
 				this.$el.append(this.template(this.model.toJSON()));
 				return this;
+			},
+
+			textCopy: function (e) {
+					var newText = $(e.currentTarget).val();
+					$('.textCopy').text(newText);
 			}
 		}, {
 
